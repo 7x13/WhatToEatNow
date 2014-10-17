0d89468ae64115f8a0e76c8fc6ee1362a1825c40
CuiYu <sxmgzyh@163.com>
2014-10-17 23:51:37 +0800

    Creat the project named WhatToEatNow
    Creat the project named WhatToEatNow

---
+++ WhatToEatNow/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WhatToEatNow
+//
+//  Created by 崔宇 on 14/10/17.
+//  Copyright (c) 2014年 cuiyu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
