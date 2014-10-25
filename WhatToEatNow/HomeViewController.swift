ef217c2f12556419129cad8408c92e077b638971
CuiYu <sxmgzyh@163.com>
2014-10-25 22:51:41 +0800

    给HomeView添加NavigationBar，修复和状态栏冲突的问题

--- WhatToEatNow/HomeViewController.swift
+++ WhatToEatNow/HomeViewController.swift
@@ -12,6 +12,9 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 解决StatusBar和程序页面重合的问题
+        self.automaticallyAdjustsScrollViewInsets = false
+        setNavigationBar()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,18 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   /**
+    * 添加NavigationBar
+    */
+    func setNavigationBar() {
+        var screenRect:CGRect = self.view.bounds
+        var navBar:UINavigationBar = UINavigationBar(frame: CGRectMake(0, 20, screenRect.size.width, 44))
+        var navItem:UINavigationItem = UINavigationItem(title: "Home")
+        navBar.pushNavigationItem(navItem, animated: true)
+        self.view.addSubview(navBar)
+        var item : UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Reply, target: self, action: "navigationBackButton:")
+        navItem.leftBarButtonItem = item
+        navBar.setItems(NSArray(object: navItem), animated: true)
+    }
 
 }
-
